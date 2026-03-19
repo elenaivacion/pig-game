1c2f281c67de522a4ddb01ceaa5517923e7c881f
Elena Ivacion <elena.ivacion@gmail.com>
2026-03-19 22:45:15 +0200

    added dummy controller file

---
+++ backend/python/controller.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/status', methods=['GET'])
+def get_status():
+    return jsonify({"message": "It Works!"})
+
+@app.route('/api/init', methods=['POST'])
+def init_game():
+    return jsonify({"func": "init_game"})
+
+@app.route('/api/roll', methods=['POST'])
+def roll_dice():
+    return jsonify({"func": "roll_dice"})
+                    
+@app.route('/api/hold', methods=['POST'])
+def hold_score():
+    return jsonify({"func": "hold_score"})
+
+# ---------------------------------------------
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
